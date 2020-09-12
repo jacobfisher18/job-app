344f454fe84718c98dfc97af466b9d886452fbdd
jacobfisher18 <jacobfisher18@gmail.com>
2020-09-11 18:49:51 -0700

    introduce prop types for the sidebar

--- job-app-client/components/sidebar.tsx
+++ job-app-client/components/sidebar.tsx
@@ -1,27 +1,31 @@
 import Link from 'next/link';
 import styles from '../styles/components/sidebar.module.scss';
-import { sidebarItems } from '../util/pages';
+import { Page, sidebarItems } from '../util/pages';
 
-export default function Sidebar(props) {
-  return (
-    <div className={styles.SidebarContainer}>
-        <img className={styles.SidebarAppLogo} src="/jaba-logo-white.png" alt="Jaba logo"/>
-        <div className={styles.SidebarItemsContainer}>
-            {
-                sidebarItems.map((item, i) => {
-                    return (
-                        <div className={styles.SidebarItem} key={i}>
-                            <img className={styles.SidebarIcon} src={item.img_src} alt={`${item.text} icon`}/>
-                            <Link href={item.link}>
-                                <p className={`${styles.SidebarText} ${item.page === props.activePage ? styles.Bolder : ""}`}>
-                                    {item.text}
-                                </p>
-                            </Link>
-                        </div>
-                    )
-                })
-            }
+export default function Sidebar(props: propTypes) {
+    return (
+        <div className={styles.SidebarContainer}>
+            <img className={styles.SidebarAppLogo} src="/jaba-logo-white.png" alt="Jaba logo" />
+            <div className={styles.SidebarItemsContainer}>
+                {
+                    sidebarItems.map((item, i) => {
+                        return (
+                            <div className={styles.SidebarItem} key={i}>
+                                <img className={styles.SidebarIcon} src={item.img_src} alt={`${item.text} icon`} />
+                                <Link href={item.link}>
+                                    <p className={`${styles.SidebarText} ${item.page === props.activePage ? styles.Bolder : ""}`}>
+                                        {item.text}
+                                    </p>
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
+}
+
+type propTypes = {
+    activePage: Page
 }
